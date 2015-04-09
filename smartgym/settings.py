7be721b6e6deffc5451b070ff46d23b74fb09af5
danieleldjarn <danieleldjarn@gmail.com>
2015-04-09 15:41:27 +0000

    Updated login redirect

--- smartgym/settings.py
+++ smartgym/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import users.settings
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -82,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/users/profile/'
