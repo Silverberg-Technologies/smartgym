4d85601192bf2c2168e7c2a07ce5453290c79809
danieleldjarn <danieleldjarn@gmail.com>
2015-04-24 12:35:19 +0000

    Updated admin view of Group sessions

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import Groupsession
 
 # Register your models here.
+class GroupsessionAdmin(admin.ModelAdmin):
+	list_display = ('name','coach', 'date_time')
+
+admin.site.register(Groupsession, GroupsessionAdmin)
--- users/models.py
+++ users/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Groupsession(models.Model):
 	name = models.CharField(max_length=100)
+	def __str__(self):
+		return self.name
 	date_time = models.DateTimeField('date and time of session')
 	coach = models.CharField(max_length=100)
 	description = models.CharField(max_length=1024)
