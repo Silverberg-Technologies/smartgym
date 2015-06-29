a3ea65fb322a7a5599cdb139fddba5b19238d953
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-06-29 14:36:09 +0000

    Test lfopen data fetching bugfix

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 
