0f9b2713a9bfed2235388060aa600685b4fc43ee
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-06-29 14:28:06 +0000

    Test lfopen data fetching bugfix

--- users/views.py
+++ users/views.py
@@ -91,7 +91,7 @@ def lfconnect(request, username):
 
 def get_lf_data(request):
     user = get_object_or_404(User, username='admin')
-    oauth = get_object_or_404(Oauth2Codes, user=user)
+    oauth = get_object_or_404(Oauth2Codes, user=user, pk=3)
     access_token = oauth.objects.values()[2]
     payload = {'access_token': access_token}
     #redirect_uri = "http://46.101.58.27:9000/users/displaydata/"
