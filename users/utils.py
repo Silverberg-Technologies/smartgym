ff369070310fb201ebdfb3db5687c7f542c0f4e7
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-06-30 17:11:44 +0000

    Refactor function that checks if user is linked to LFOpen

--- users/utils.py
+++ users/utils.py
@@ -4,15 +4,13 @@ from users.models import Oauth2Codes
 
 def is_lf_connected(username):
     user = get_object_or_404(User, username=username)
-    oauth = get_object_or_404(Oauth2Codes, user=user)
-    return True if oauth else False
-    #try:
+    try:
     #    user = get_object_or_404(User, username=username)
     #    try:
-    #        oauth = Oauth2Codes.objects.get(user=user)
-    #        return True
-    #    except Oauth2Codes.DoesNotExist:
-    #        return False
+        oauth = Oauth2Codes.objects.get(user=user)
+        return True
+    except Oauth2Codes.DoesNotExist:
+        return False
     #except User.DoesNotExist:
     #    return False
 
