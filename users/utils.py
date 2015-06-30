26ef9691e06c4f32b636b34f0924956eb9f1f89c
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-06-30 14:42:48 +0000

    LFOpen connect button is only visible if user is not connected

--- users/utils.py
+++ users/utils.py
@@ -4,10 +4,10 @@ from users.models import Oauth2Codes
 def is_lf_connected(username):
     try:
         user = User.objects.get(username=username)
-        print user
+        print(user)
         try:
             oauth = Oauth2Codes.objects.get(user=user)
-            print oauth
+            print(oauth)
             return True
         except Oauth2Codes.DoesNotExist:
             return False
