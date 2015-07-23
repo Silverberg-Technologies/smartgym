0fdac65bbf60b147e2a3ca6da3fabc9689c094dd
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-07-23 12:32:52 +0000

    Testing dynamic editing of profile

--- users/static/users/js/profile.js
+++ users/static/users/js/profile.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $("p").click(function() {
         $(this).hide();
     });
+    $("button#modeswitch").click(function() {
+        $("div.viewmode").hide();
+        $("div.editmode").show();
+    });
 });
