a9d9ef3967aaed75d8fc5212c79b01f3fd8ba5b4
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-07-28 14:52:37 +0000

    Testing SmartGym User model extension

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from users.models import Groupsession
+from users.models import SmartGymUser
 
 # Register your models here.
 class GroupsessionAdmin(admin.ModelAdmin):
