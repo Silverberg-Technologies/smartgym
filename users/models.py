2795e22861eda79bd331daa4e9f7fedbdaa77dae
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-07-30 14:39:54 +0000

    Testing improved groupsession model

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class Groupsession(models.Model):
     name = models.CharField(max_length=128)
     date_time = models.DateTimeField('date and time of session')
     location = models.CharField(max_length=128)
-    instructor = models.OneToOneField(SmartGymUser, default = lambda: SmartGymUser.objects.get(id=1))
+    instructor = models.OneToOneField(User)
     description = models.CharField(max_length=1024)
     users_attending = models.ManyToManyField(User)
     available_slots = models.IntegerField()
