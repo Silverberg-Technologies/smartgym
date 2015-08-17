b2d588d526f7c7cf8fac0df85c71651c86615e4c
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-08-17 11:19:40 +0000

    Included testing of SmartGymUser model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import SmartGymUser
+from django.contrib.auth.models import User
+
+class SmartGymUserCreationTest(TestCase):
+    def test_smartgymuser_creation(self):
+        user = User(username="TestUser")
+        self.assertNotEqual(user.smartgymuser, None)
+    
